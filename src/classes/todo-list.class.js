1b57c74cbdd72ee836e43136017a2acabdfd589e
Slater <armando.jaimes@pucp.edu.pe>
2022-01-22 01:00:49 -0500

    Eliminar ToDos completados

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -38,7 +38,7 @@ export class TodoList {
 
     eliminarCompletados() {
         
-        // this.todos = this.todos.filter( todo => !todo.completado )
+        this.todos = this.todos.filter( todo => !todo.completado )
         // this.guardarLocalStorage();
     }
 

c299b2ec039fa856d98e62ed1b9273c7abdca79f
Slater <armando.jaimes@pucp.edu.pe>
2022-01-22 01:35:13 -0500

    Guardando y recuperando ToDos

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -5,19 +5,19 @@ export class TodoList {
     constructor() {
 
         this.todos = [];
-        // this.cargarLocalStorage();
+        this.cargarLocalStorage();
 
     }
 
     nuevoTodo( todo ) {
         this.todos.push( todo );
-        // this.guardarLocalStorage();
+        this.guardarLocalStorage();
     }
 
     eliminarTodo( id ) {
 
         this.todos = this.todos.filter( todo => todo.id != id )
-        // this.guardarLocalStorage();
+        this.guardarLocalStorage();
     }
 
     marcarCompletado( id ) { 
@@ -27,7 +27,7 @@ export class TodoList {
             if( todo.id == id ) {
 
                 todo.completado = !todo.completado;
-                // this.guardarLocalStorage();
+                this.guardarLocalStorage();
                 break;
             }
 
@@ -39,20 +39,20 @@ export class TodoList {
     eliminarCompletados() {
         
         this.todos = this.todos.filter( todo => !todo.completado );
-        // this.guardarLocalStorage();
+        this.guardarLocalStorage();
     }
 
     guardarLocalStorage(){
 
-        // localStorage.setItem('todo', JSON.stringify( this.todos ) );
+        localStorage.setItem('todo', JSON.stringify( this.todos ) );
         
     }
 
     cargarLocalStorage(){
 
-        // this.todos = ( localStorage.getItem('todo') )
-        //                 ? JSON.parse( localStorage.getItem('todo') )
-        //                 : [];
+        this.todos = ( localStorage.getItem('todo') )
+                        ? JSON.parse( localStorage.getItem('todo') )
+                        : [];
         
         // this.todos = this.todos.map( Todo.fromJson );
     }
